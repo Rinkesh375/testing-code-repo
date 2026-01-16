515874004f0e864cfc6f102279bd6dd6f751e53d
rinkesh375 <rinkeshujjwal16@gmail.com>
2026-01-16 20:43:49 +0530

    added third optimized logic

--- app.js
+++ app.js
@@ -25,3 +25,23 @@ function isPrime(n) {
 }
 
 console.log(isPrime(29)); // true
+
+
+
+//third optimized way
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+
+  const limit = Math.sqrt(n);
+
+  for (let i = 3; i <= limit; i += 2) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+console.log(isPrime(29)); // true
+console.log(isPrime(49)); // false
