40380a658bb9d0b6b48e0be0eb6004b72c52acbb
rinkesh375 <rinkeshujjwal16@gmail.com>
2026-01-16 20:30:44 +0530

    prime number to findout

---
+++ app.js
@@ -0,0 +1,27 @@
+function isPrime(n) {
+  if (n <= 1) return false;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(7));  // true
+console.log(isPrime(10)); // false
+
+
+function isPrime(n) {
+  if (n <= 1) return false;
+
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(29)); // true
